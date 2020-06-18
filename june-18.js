5ae6b6274846cf77579b29c920fa3ba6180f82c5
Urmil Bhatt <yudi.bhatt12@gmail.com>
2020-06-18 22:06:13 +0530

    Solution for the challenge on June 18th.

---
+++ june-18.js
@@ -0,0 +1,50 @@
+// H-Index II
+/*
+Given an array of citations sorted in ascending order (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+Example:
+
+Input: citations = [0,1,3,5,6]
+Output: 3 
+Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each of them had 
+             received 0, 1, 3, 5, 6 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each and the remaining 
+             two with no more than 3 citations each, her h-index is 3.
+Note:
+
+If there are several possible values for h, the maximum one is taken as the h-index.
+
+Follow up:
+
+This is a follow up problem to H-Index, where citations is now guaranteed to be sorted in ascending order.
+Could you solve it in logarithmic time complexity?
+   Hide Hint #1  
+Expected runtime complexity is in O(log n) and the input is sorted.
+*/
+
+var hIndex = function(citations) {
+    let len = citations.length;
+  
+    citations.unshift(0);
+    let low = 0;
+    let high = len;
+    let mid;
+    let max = 0;
+    while (low <= high) {
+      mid = (low + high) >> 1;
+  
+      // numbers count >= citations[mid]
+      let largetLeft = len - mid + 1;
+      
+      if (citations[mid] >= largetLeft) {
+        max = Math.max(max, largetLeft);
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+  
+    return max;
+  };
